64d6e282145fabd8b98cb994692ee0b316b07319
CollidingAdversaries <tsaala@uni-bonn.de>
2025-04-04 16:06:59 +0200

    add mobile support

--- script.js
+++ script.js
@@ -4,26 +4,23 @@ $(document).ready(function () {
     // Disable built-in CSS smooth scrolling to prevent conflicts
     $('html').css('scroll-behavior', 'auto');
 
+    // Scroll to section
     $('a.scrollto').on('click', function (e) {
         e.preventDefault(); // Prevent default jump behavior
-
         var target = this.hash;
         if ($(target).length) {
             var $target = $(target);
-
-            // Calculate target scroll position
             var scrollto = $target.offset().top - headerHeight;
-
-            // Stop ongoing animations if necessary
             $('html, body').stop();
-
-            // Perform smooth scrolling
             $('html, body').animate({
                 scrollTop: scrollto
             }, 500, 'easeInOutCubic');
-
-            // Replace hash in URL without affecting scroll position
             history.replaceState(null, null, window.location.pathname + window.location.search);
         }
+    });
+
+    // Mobile menu toggle
+    $('#mobile-menu').on('click', function () {
+        $('ul.nav-links').toggleClass('active');
     });
 });
