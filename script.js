63f9e2b2ccabbf7e9be732faba6a5edd36b28d37
CollidingAdversaries <tsaala@uni-bonn.de>
2025-04-04 14:35:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ $(document).ready(function () {
     var $header = $('#header');
     var headerHeight = $header.outerHeight();
 
-    // Check if header height is being calculated correctly
-    console.log(headerHeight);
-
     // Smooth scrolling for navigation menu links with 'scrollto' class
     $('a.scrollto').on('click', function (e) {
         var target = this.hash;
@@ -15,46 +12,13 @@ $(document).ready(function () {
             // Calculate scroll position considering the header height
             var scrollto = $(target).offset().top - headerHeight;
 
+            // Log to check scroll position
+            console.log("Scroll To: ", scrollto);
+
             // Smooth scroll to the target position
             $('html, body').animate({
                 scrollTop: scrollto
             }, 1500, 'easeInOutExpo');
         }
-    });
-
-    // Highlight active nav items on scroll
-    $(window).on('scroll', function () {
-        var cur_pos = $(this).scrollTop() + 10; // Offset for smoother tracking
-
-        $('section').each(function () {
-            var top = $(this).offset().top - headerHeight,
-                bottom = top + $(this).outerHeight();
-
-            // Check if current scroll position is within the section
-            if (cur_pos >= top && cur_pos <= bottom) {
-                // Remove active class from all nav items
-                $('.nav-menu li').removeClass('active');
-
-                // Add active class to the corresponding nav item
-                $('.nav-menu a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-            }
-        });
-    });
-
-    // Back to top button visibility
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-
-    // Back to top button click action
-    $('.back-to-top').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1500, 'easeInOutExpo');
-        return false;
     });
 });
